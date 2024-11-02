31133c7e4a8d17520747d18952e11a9a44dab5c5
akashdeep023 <ad3500476@gmail.com>
2024-11-02 20:26:26 +0530

    Add Animation

--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const ProjectBox = ({ project }) => {
 	return (
-		<div className="relative group flex flex-col gap-2 overflow-hidden bg-gradient-to-t to-customeLight-500 dark:to-customeDark-500 from-customeLight-600 dark:from-customeDark-600 px-3 py-5 min-h-60 sm:min-h-64 md:min-h-72 rounded-3xl border dark:border-white/40 border-black/40">
+		<div className="projectB relative group flex flex-col gap-2 overflow-hidden bg-gradient-to-t to-customeLight-500 dark:to-customeDark-500 from-customeLight-600 dark:from-customeDark-600 px-3 py-5 min-h-60 sm:min-h-64 md:min-h-72 rounded-3xl border dark:border-white/40 border-black/40">
 			<h2 className="text-center text-xl sm:text-2xl font-bold">
 				{project.name}
 			</h2>
 			<div className="h-20 w-20 blur-3xl z-0  absolute top-0 right-0 bg-gradient-to-t to-customeGreen-500 from-customeGreen-600"></div>
 			<div className="h-20 w-20 blur-3xl z-0  absolute bottom-0 left-0 bg-gradient-to-t to-customeGreen-500 from-customeGreen-600"></div>
-			<div className="w-full z-10">
+			<div className="w-full z-10 projectB">
 				<img
 					src={"/projects/" + project.image}
 					alt={project.name}
@@ -31,8 +31,8 @@ const ProjectBox = ({ project }) => {
 					);
 				})}
 			</p>
-			<div className="backdrop-blur-sm z-10 h-full w-full absolute top-0 left-0 flex transition-all duration-700 items-center group-hover:opacity-100 opacity-0">
-				<div className="p-2 flex justify-center gap-4 w-full">
+			<span className="backdrop-blur-sm z-10 h-full w-full absolute top-0 left-0 flex transition-all duration-700 items-center group-hover:opacity-100 opacity-0">
+				<span className="p-2 flex justify-center gap-4 w-full">
 					{project?.live && (
 						<Link
 							target="_blank"
@@ -60,8 +60,8 @@ const ProjectBox = ({ project }) => {
 							<FaLinkedinIn />
 						</Link>
 					)}
-				</div>
-			</div>
+				</span>
+			</span>
 		</div>
 	);
 };
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import Heading from "../components/Heading";
 import { projectDetails } from "../utils/constant";
 import ProjectBox from "../components/ProjectBox";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+gsap.registerPlugin(ScrollTrigger);
 
 const Project = () => {
+	const projectBox = useRef();
+	useGSAP(() => {
+		gsap.from("#projectBox div ", {
+			duration: 5,
+			opacity: 0,
+			y: 50,
+			stagger: 0.9,
+			animation: "easeOutQuad",
+			scrollTrigger: {
+				trigger: "#projectBox",
+				scroller: "body",
+				start: "top 130%",
+				end: "top -150%",
+				scrub: 3,
+			},
+		});
+	});
 	return (
 		<div className="w-full min-full mb-4 sm:mb-8">
 			<Heading text={"Project"} />
-			<div className="grid grid-flow-row sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full py-4 sm:py-5 md:py-6 px-6 sm:px-9 md:px-12">
+			<div
+				ref={projectBox}
+				id="projectBox"
+				className="grid grid-flow-row sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full py-4 sm:py-5 md:py-6 px-6 sm:px-9 md:px-12"
+			>
 				{projectDetails.map((project) => {
 					return <ProjectBox project={project} key={project.name} />;
 				})}
