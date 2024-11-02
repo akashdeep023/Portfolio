9e35b066f8e9eec02267843aa19090dc3920196e
akashdeep023 <ad3500476@gmail.com>
2024-11-03 01:17:37 +0530

    Modify Score-fam

---
+++ src/components/ScoreFam.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const ScoreFam = () => {
+	useGSAP(() => {
+		gsap.from(".score-fam > div ", {
+			duration: 5,
+			opacity: 0,
+			y: 100,
+			stagger: 0.8,
+			animation: "easeOutQuad",
+			scrollTrigger: {
+				trigger: ".score-fam",
+				scroller: "body",
+				start: "top 80%",
+				end: "top 20%",
+				scrub: 2,
+			},
+		});
+		gsap.from(".score-fam > div b ", {
+			duration: 1,
+			animation: "easeOutQuad",
+			innerText: "0",
+			snap: {
+				innerText: 0.1,
+			},
+			scrollTrigger: {
+				trigger: ".score-fam >div",
+				scroller: "body",
+				start: "top 80%",
+				end: "top 70%",
+				scrub: 5,
+			},
+		});
+	});
+	return (
+		<div className="score-fam w-full h-fit flex flex-wrap justify-evenly py-4 sm:py-5 md:py-6 px-6 sm:px-9 md:px-12">
+			<div className="size-36 sm:size-44 md:size-52 lg:size-60 m-2 sm:m-4 p-2  rounded-3xl border dark:border-white/40 border-black/40 shadow-md shadow-black/50 bg-gradient-to-t to-customeLight-500 dark:to-customeDark-500 from-customeLight-600 dark:from-customeDark-600  flex items-center text-center justify-center flex-col">
+				<div className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+					<b>22</b>
+					<span>+</span>
+				</div>
+				<h1 className="uppercase text-base sm:text-lg md:text-xl">
+					Successfully completed project
+				</h1>
+			</div>
+			<div className="size-36 sm:size-44 md:size-52 lg:size-60 m-2 sm:m-4 p-2  rounded-3xl border dark:border-white/40 border-black/40 shadow-md shadow-black/50 bg-gradient-to-t to-customeLight-500 dark:to-customeDark-500 from-customeLight-600 dark:from-customeDark-600  flex items-center text-center justify-center flex-col">
+				<div className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+					<b>15</b>
+					<span>+</span>
+				</div>
+				<h1 className="uppercase text-base sm:text-lg md:text-xl">
+					Technologies
+				</h1>
+			</div>
+			<div className="size-36 sm:size-44 md:size-52 lg:size-60 m-2 sm:m-4 p-2  rounded-3xl border dark:border-white/40 border-black/40 shadow-md shadow-black/50 bg-gradient-to-t to-customeLight-500 dark:to-customeDark-500 from-customeLight-600 dark:from-customeDark-600  flex items-center text-center justify-center flex-col">
+				<div className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+					<b>2.2</b>
+					<span>K+</span>
+				</div>
+				<h1 className="uppercase text-base sm:text-lg md:text-xl">
+					LinkedIn Family
+				</h1>
+			</div>
+			<div className="size-36 sm:size-44 md:size-52 lg:size-60 m-2 sm:m-4 p-2  rounded-3xl border dark:border-white/40 border-black/40 shadow-md shadow-black/50 bg-gradient-to-t to-customeLight-500 dark:to-customeDark-500 from-customeLight-600 dark:from-customeDark-600  flex items-center text-center justify-center flex-col">
+				<div className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+					<b>1.0</b>
+					<span>+</span>
+				</div>
+				<h1 className="uppercase text-base sm:text-lg md:text-xl">
+					Year of Experience
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default ScoreFam;
