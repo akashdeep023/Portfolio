d11a2a1a1ef8d86d607607b37f962657f38c44a7
akashdeep023 <ad3500476@gmail.com>
2024-11-02 19:08:26 +0530

    Update Details & Add Image

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,6 @@ export const contactDetails = {
 	email: "contact.akashdeep023@gmail.com",
 	phone: "+91 6207524516",
 };
-
 export const projectDetails = [
 	{
 		name: "Chat App",
@@ -11,8 +10,9 @@ export const projectDetails = [
 		github: "https://github.com/akashdeep023/Chat_App",
 		linkedin:
 			"https://www.linkedin.com/posts/akashdeep023_chatapp-mernabrstack-mernstack-activity-7222507586980388864-XiV0?utm_source=share&utm_medium=member_desktop",
-		image: "img",
-		description: "Chat Application Using MERN Stack with Tailwind CSS",
+		image: "chat-app.png",
+		description:
+			"A real-time chat application built with MERN Stack and Tailwind CSS, featuring individual and group chat functionality, user authentication, and state management.",
 		technology: [
 			"React.js",
 			"Redux",
@@ -31,9 +31,9 @@ export const projectDetails = [
 		github: "https://github.com/akashdeep023/Airbnb_Project",
 		linkedin:
 			"https://www.linkedin.com/posts/akashdeep023_airbnb-apnacollege-fullstackdevelopment-activity-7153710900992970752-Z9_G?utm_source=share&utm_medium=member_desktop",
-		image: "img",
+		image: "airbnb.png",
 		description:
-			"A feature-rich full-stack web application inspired by Airbnb.",
+			"A full-stack web application inspired by Airbnb, offering users the ability to browse, book, and review rental listings, complete with secure authentication and integrated map views.",
 		technology: [
 			"EJS",
 			"Node.js",
@@ -53,37 +53,38 @@ export const projectDetails = [
 		github: "https://github.com/akashdeep023/Food_Plaza",
 		linkedin:
 			"https://www.linkedin.com/posts/akashdeep023_foodplaza-namastedev-namastereact-activity-7168890857792761856-qC0D?utm_source=share&utm_medium=member_desktop",
-		image: "img",
+		image: "food-plaza.png",
 		description:
-			"Welcome to Food Plaza, a cutting-edge food delivery app designed to revolutionize your dining experience. Get ready to enjoy seamless food ordering with a user-friendly interface and personalized features.",
+			"A user-friendly food delivery app offering seamless browsing, ordering, and tracking of meals with a personalized experience for customers.",
 		technology: ["React.js", "Redux", "CSS", "JavaScript"],
 		project: "Major Project",
 	},
 	{
-		name: "Netflix Gpt",
+		name: "Netflix GPT",
 		live: "https://netflix-gpt-jack.vercel.app/",
 		github: "https://github.com/akashdeep023/Netflix_Gpt",
 		linkedin:
 			"https://www.linkedin.com/posts/akashdeep023_netflixgpt-openai-gpt-activity-7172444217913360385-pPZ4?utm_source=share&utm_medium=member_desktop",
-		image: "img",
+		image: "netflix-gpt.png",
 		description:
-			"Greetings, movie lovers! Welcome to Netflix-GPT, a state-o-the-art movie recommendation website powered by the GPT model and Firebase.",
+			"A movie recommendation platform powered by GPT technology and Firebase, designed to help users discover new movies and series based on their interests.",
 		technology: [
 			"React.js",
 			"Redux",
 			"TailwindCSS",
 			"JavaScript",
-			"FireBase",
+			"Firebase",
 		],
 		project: "Major Project",
 	},
 	{
-		name: "Cors Handler",
+		name: "CORS Handler",
 		live: "https://cors-handlers.vercel.app/",
 		linkedin:
 			"https://www.linkedin.com/posts/akashdeep023_webdevelopment-nodejs-expressjs-activity-7184854679493783553-dFXF?utm_source=share&utm_medium=member_desktop",
-		image: "img",
-		description: "--------------------------------",
+		image: "cors-handler.png",
+		description:
+			"A web utility for handling CORS (Cross-Origin Resource Sharing) issues in APIs, built using Node.js and Express.js.",
 		technology: [
 			"EJS",
 			"Node.js",
@@ -97,8 +98,9 @@ export const projectDetails = [
 	{
 		name: "Code With",
 		live: "https://code-with.vercel.app/",
-		image: "img",
-		description: "--------------------------------",
+		image: "code-with.png",
+		description:
+			"A collaborative coding platform for developers to share code snippets with others, built using MERN stack and Tailwind CSS.",
 		technology: [
 			"React.js",
 			"Node.js",
@@ -112,16 +114,18 @@ export const projectDetails = [
 	{
 		name: "Drioh Project",
 		live: "https://drioh.vercel.app/",
-		image: "img",
-		description: "--------------------------------",
+		image: "drioh.png",
+		description:
+			"An interactive project focused on UI/UX using React and Tailwind CSS for responsive design.",
 		technology: ["React.js", "TailwindCSS", "JavaScript"],
 		project: "Client Project",
 	},
 	{
 		name: "Quable Food",
 		live: "https://quablefoods.vercel.app/",
-		image: "img",
-		description: "--------------------------------",
+		image: "quable.png",
+		description:
+			"A masala delivery service website featuring an intuitive interface and responsive design, built using React and Tailwind CSS.",
 		technology: ["React.js", "TailwindCSS", "JavaScript"],
 		project: "Client Project",
 	},
@@ -129,8 +133,9 @@ export const projectDetails = [
 		name: "Life Track",
 		live: "https://todo-mern-project.vercel.app/",
 		github: "https://github.com/akashdeep023/Todo_Mern",
-		image: "img",
-		description: "--------------------------------",
+		image: "life-track.png",
+		description:
+			"A productivity application to manage daily tasks, featuring a clean interface and built with the MERN stack.",
 		technology: [
 			"React.js",
 			"Node.js",
@@ -145,18 +150,19 @@ export const projectDetails = [
 		name: "Weather Widget",
 		live: "https://weather-widget-react-jack.netlify.app/",
 		github: "https://github.com/akashdeep023/Weather_Widget",
-		image: "img",
-		description: "This is a simple weather app.",
+		image: "weather.png",
+		description:
+			"A simple weather application providing current weather details based on location, built with React and Material UI.",
 		technology: ["React.js", "MaterialUI", "HTML", "CSS", "JavaScript"],
 		project: "Mini Major Project",
 	},
 	{
-		name: "SimonSay Game Js",
+		name: "SimonSay Game",
 		live: "https://akashdeep023.github.io/SimonSay_Game/",
 		github: "https://github.com/akashdeep023/SimonSay_Game",
-		image: "img",
+		image: "simon-say.png",
 		description:
-			"Simon Says is a classic memory game where players must remember and repeat a sequence of colors and sounds. This project is a web-based implementation of the Simon Says Game.",
+			"A web-based implementation of the classic memory game 'Simon Says,' challenging players to recall and repeat color sequences.",
 		technology: ["HTML", "CSS", "JavaScript"],
 		project: "Mini Major Project",
 	},
@@ -164,9 +170,9 @@ export const projectDetails = [
 		name: "Amazon Clone",
 		live: "https://akashdeep023.github.io/Amazon-Clone/",
 		github: "https://github.com/akashdeep023/Amazon-Clone",
-		image: "img",
+		image: "amazon.png",
 		description:
-			"A clone of the Spotify website for practicing web development skills.",
+			"A frontend clone of the Amazon website built to practice and improve web development skills.",
 		technology: ["HTML", "CSS", "JavaScript"],
 		project: "Mini Major Project",
 	},
@@ -174,9 +180,9 @@ export const projectDetails = [
 		name: "Spotify Clone",
 		live: "https://akashdeep023.github.io/Spotify-Clone/",
 		github: "https://github.com/akashdeep023/Spotify-Clone",
-		image: "img",
+		image: "spotify.png",
 		description:
-			"A clone of the Spotify website for practicing web development skills.",
+			"A frontend clone of the Spotify website, created to practice HTML and CSS skills.",
 		technology: ["HTML", "CSS"],
 		project: "Mini Major Project",
 	},
