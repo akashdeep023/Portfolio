4e17f7185a54b16a804346479492a2b947bed79c
akashdeep023 <ad3500476@gmail.com>
2024-11-03 21:32:35 +0530

    Update name

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -9,14 +9,14 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Project = () => {
 	useGSAP(() => {
-		gsap.from("#projectBox div", {
+		gsap.from("#projectCont div", {
 			duration: 3,
 			opacity: 0,
 			y: 80,
 			stagger: 0.8,
 			animation: "easeOutQuad",
 			scrollTrigger: {
-				trigger: "#projectBox",
+				trigger: "#projectCont",
 				scroller: "body",
 				start: "top 125%",
 				end: "bottom 125%",
@@ -28,7 +28,7 @@ const Project = () => {
 		<div className="w-full min-full mb-4 sm:mb-8">
 			<Heading text={"Project"} />
 			<div
-				id="projectBox"
+				id="projectCont"
 				className="grid grid-flow-row sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full py-4 sm:py-5 md:py-6 px-6 sm:px-9 md:px-12"
 			>
 				{projectDetails.map((project) => {
