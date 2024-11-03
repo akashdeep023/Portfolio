9f0a368b039051f5b49f2a88af1b9373704bc2f8
akashdeep023 <ad3500476@gmail.com>
2024-11-04 04:07:37 +0530

    Update btn

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { BsArrowLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
 	return (
 		<div className="w-full h-[calc(100vh-60px)] flex flex-col gap-2 items-center justify-center">
-			<h1 className="text-xl sm:text-2xl font-bold">Page Not Found</h1>
+			<h1 className="text-xl sm:text-2xl font-bold -mt-32">
+				Page Not Found
+			</h1>
 			<h1 className="text-lg sm:text-3xl font-bold">404 | Bad request</h1>
 			<p className="leading-7 opacity-90">Go back to Home Page</p>
-			<Link to={"/"}>Home</Link>
+			<Link
+				to={"/"}
+				className="group shadow-lg hover:shadow-xl bg-gradient-to-l hover:bg-gradient-to-r to-customeGreen-500 from-customeGreen-600 text-black h-10 w-fit py-2 px-6 rounded-lg flex items-center justify-cente gap-3 text-sm sm:text-base"
+			>
+				<BsArrowLeft className="group-hover:-translate-x-2/3 transition-all " />
+				<span>Back to Home</span>
+			</Link>
 		</div>
 	);
 };
